23072b8b698ac9c8c7186eafa534e80384b21072
Philip Cortes <philip.cortes@gmail.com>
2013-03-22 19:02:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomzer::Application.config.secret_token = 'be8d36cfed9d6aaff0a0a5521bc9b6e3f39785a7decb10eb1f3a91a748f3054149559715842da6a20f99404f840f7e67f0be012157f93a7e3009a5b74b058769'
